8ce13b43359b66cfcfc191d4bd5a88e8a78ec3a9
Victoria Kruczek <victoria.kruczek@gmail.com>
2024-08-27 16:38:20 +0200

    Modify status validation with array inclusion

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :goat
 
-  validates :status, :total_price, presence: true
+  validates :status, presence: true, inclusion: { in: %w[pending approved rejected], message: "%{value} is not a valid status" }
+  validates :total_price, presence: true
 end
