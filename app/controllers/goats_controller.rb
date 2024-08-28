5fe0989e4f922a950cbc54cbdf2638e5e739dde2
Romain Berrou <romain.a.berrou@gmail.com>
2024-08-28 13:03:26 +0200

    add goat etc

--- app/controllers/goats_controller.rb
+++ app/controllers/goats_controller.rb
@@ -1,5 +1,42 @@
 class GoatsController < ApplicationController
+
+  before_action :set_goat, only: [:show]
+
   def show
     @goat = Goat.find(params[:id])
+  end
+
+  def new
+    @goat = Goat.new
+  end
+
+  def create
+    @goat = Goat.new(goat_params)
+    @goat.user = current_user # Assuming you have user authentication and each goat belongs to a user
+
+    if @goat.save
+      redirect_to root_path, notice: 'Goat was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def random
+    random_goat = Goat.order("RANDOM()").first
+    if random_goat
+      redirect_to goat_path(random_goat)
+    else
+      redirect_to root_path, alert: 'No goats available.'
+    end
+  end
+
+  private
+
+  def set_goat
+    @goat = Goat.find(params[:id])
+  end
+
+  def goat_params
+    params.require(:goat).permit(:name, :price_per_day, :bio, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root to: "pages#index"
-
   get 'dashboard', to: 'pages#dashboard'
 
-  resources :goats, only: [:show] do
+  resources :goats, only: [:show, :new, :create] do
+    collection do
+      get 'random', to: 'goats#random', as: 'random'
+    end
     resources :bookings, only: [:new, :create]
   end
 end
