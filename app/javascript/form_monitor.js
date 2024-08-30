5fdd6f548da0b1e377d3a219736560aa18d1281b
louis whittleadams <louiswhittle7d1@gmail.com>
2024-08-30 11:07:26 +0100

    js on the back burner, dashboard file made and changes added

--- app/javascript/form_monitor.js
+++ app/javascript/form_monitor.js
@@ -1,9 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+/* console.log('form_monitor.js loaded');
+const linkElement = document.getElementById('rent-your-goat-link');
+
+linkElement.addEventListener('click', function() {
+  console.log('Link clicked');
   const form = document.getElementById('goat-add-form');
   const submitBtn = document.getElementById('goat-add-submit');
 
   function checkFormFilled() {
-     const requiredFields = form.querySelectorAll('[required]');
+    const requiredFields = form.querySelectorAll('[required]');
     let allFilled = true;
 
     requiredFields.forEach(function(field) {
@@ -11,17 +15,28 @@ document.addEventListener("DOMContentLoaded", function() {
         allFilled = false;
       }
     });
-    console.log(allFilled)
+
+    console.log(allFilled);
+
     if (allFilled) {
       submitBtn.classList.add('all-filled');
-      submitBtn.desabled = false;
+      submitBtn.disabled = false;
+      localStorage.setItem('formFilled', 'true');
     } else {
       submitBtn.classList.remove('all-filled');
-      submitBtn.desabled = true;
+      submitBtn.disabled = true;
+      localStorage.setItem('formFilled', 'false');
     }
   }
+  console.log(form) */
+  /* form.addEventListener('input', checkFormFilled); */
 
-  form.addEventListener('input', checkFormFilled);
+  // Check if the form was previously filled
+/*   const isFormFilled = localStorage.getItem('formFilled');
+  if (isFormFilled === 'true') {
+    submitBtn.classList.add('all-filled');
+    submitBtn.disabled = false;
+  }
 
   checkFormFilled();
-});
+}); */
